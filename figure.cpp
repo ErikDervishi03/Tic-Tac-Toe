d68550e6e9836d901c213adb00b77e36f74fb7d0
Artem <artrom170@gmail.com>
2023-06-06 22:05:01 +0300

    Figure creation

--- figure.cpp
+++ figure.cpp
@@ -1,1 +1,31 @@
 #include "figure.h"
+bool Figure::isVisible() const
+{
+  return isVisible_;
+}
+void Figure::setVisibility(bool status)
+{
+  isVisible_ = status;
+}
+Figure::Figure():
+  isVisible_(false),
+  pos_(0, 0),
+  size_(0)
+{
+}
+sf::Vector2i Figure::getPos() const
+{
+  return pos_;
+}
+void Figure::setPos(sf::Vector2i rhs)
+{
+  pos_ = rhs;
+}
+size_t Figure::getSize() const
+{
+  return size_;
+}
+void Figure::setSize(size_t rhs)
+{
+  size_ = rhs;
+}
--- figure.h
+++ figure.h
@@ -1,6 +1,21 @@
 #ifndef TICIACTOEGAME_FIGURE_H
 #define TICIACTOEGAME_FIGURE_H
+#include <SFML/System/Vector2.hpp>
+#include <cstddef>
 class Figure
 {
+public:
+  Figure();
+  virtual ~Figure() = 0; //TODO
+  bool isVisible() const;
+  void setVisibility(bool status);
+  sf::Vector2i getPos() const;
+  void setPos(sf::Vector2i rhs);
+  size_t getSize() const;
+  void setSize(size_t rhs);
+private:
+  bool isVisible_;
+  sf::Vector2i pos_;
+  size_t size_;
 };
 #endif
